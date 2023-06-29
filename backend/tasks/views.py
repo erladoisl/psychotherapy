a213104e6d3ef680b05e9dfcddc8b1f9a0744c38
erladoisl <erladoisl@gmail.com>
2023-06-29 15:13:55 +0300

    logging

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -5,19 +5,30 @@ from rest_framework.permissions import IsAuthenticated
 from django.http import Http404
 from .serializers import UnappropriateEventSerializer
 from .models import UnappropriateEvent
+import config as c
+import traceback
+import logging
 
+fh = logging.FileHandler(c.LOGGER_CONFIG['file'])
+fh.setFormatter(c.LOGGER_CONFIG['formatter'])
+log = logging.getLogger('tasks.view')
+log.addHandler(fh)
+log.setLevel(c.LOGGER_CONFIG['level'])
 
 class UnappropriateEmotionsView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get_object(self, pk):
         try:
+            log.info(f'Unappropriate Event by pk: {pk}')
             return UnappropriateEvent.objects.get(pk=pk)
         except UnappropriateEvent.DoesNotExist:
+            log.error(f'Unappropriate Event [{pk}] does not exist')
             raise Http404
 
     def get(self, request):
         ''' Returns authenticated user's unappropriate_event list '''
+        log.info('getting users Unappropriate Events')
         return Response(UnappropriateEventSerializer(UnappropriateEvent.objects.filter(user=request.user).order_by('-created_at')[:10], many=True).data)
 
     def post(self, request, *args, **kwargs):
@@ -26,6 +37,11 @@ class UnappropriateEmotionsView(APIView):
         feelings_actions = request.data.get('feelings_actions', '')
         desired_feelings_actions = request.data.get(
             'desired_feelings_actions', '')
+        
+        log.info(f'Creating a New Unappropriate Event')
+        log.debug(f'situation: {situation}')
+        log.debug(f'feelings_actions: {feelings_actions}')
+        log.debug(f'desired_feelings_actions: {desired_feelings_actions}')
 
         if situation and feelings_actions and desired_feelings_actions:
             unappropriate_event = UnappropriateEvent.objects.create(
@@ -33,8 +49,12 @@ class UnappropriateEmotionsView(APIView):
                 situation=situation,
                 feelings_actions=feelings_actions,
                 desired_feelings_actions=desired_feelings_actions)
+            
+            log.info('New Unappropriate Event successfully created')
 
             return Response(UnappropriateEventSerializer(unappropriate_event).data, status=status.HTTP_201_CREATED)
+        else:
+            log.info("Invalid Event's data")
 
         return Response({'err': 'Ошибка ввода данных'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -43,5 +63,7 @@ class UnappropriateEmotionsView(APIView):
         uuid = request.data.get('uuid', '')
         unappropriate_event = UnappropriateEvent.objects.get(uuid=uuid)
         unappropriate_event.delete()
+        
+        log.info(f"Unappropriate Event [{uuid}] successfully deleted")
 
         return Response(status=status.HTTP_204_NO_CONTENT)
